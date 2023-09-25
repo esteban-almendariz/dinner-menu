4385cd6ca99f47dd5fd724ece1639460559b317b
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2023-09-24 20:23:08 -0500

    completed project and details

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 const menuArray = [
     {
         name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        ingredients: ["pepperoni, mushrom, mozarella"],
         id: 0,
         price: 14,
         emoji: '/images/pizza.png'
     },
     {
         name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
+        ingredients: ["beef, cheese, lettuce"],
         price: 12,
         emoji: '/images/burger.png',
         id: 1
--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import menuArray from '/data'
 
 const listCartItems = document.getElementById('items')
 const totalPriceEle = document.getElementById('total-price')
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.getElementById("complete-order-btn");
+const closeModalBtn = document.querySelector(".btn-close");
 let totalPrice = 0
 
 
@@ -11,9 +15,9 @@ document.getElementById('menu').innerHTML += menuArray.map(item => {
     return `
     <div class="item-container" id=${id}>
                 <img src=${emoji} class="emoji">
-                <div>
+                <div class='detail-container'>
                     <h2>${name}</h2>
-                    <p>${ingredients}</p>
+                    <p class='ingredients'>${ingredients}</p>
                     <p>$${price}</p>
                 </div>
                 <button class="increment-btn" id='${id}'>+</button>
@@ -30,7 +34,7 @@ const handleAddItem = (e) => {
                           <div class="cart-container">
                               <p>${filterItem[0].name}</p>
                               <button class="remove-btn" id='${filterItem[0].id}'>remove</button>
-                              <span>$${filterItem[0].price}</span>
+                              <p class='order-price'>$${filterItem[0].price}</p>
                           </div> 
           `
           menuArray.map(item => {
@@ -45,25 +49,43 @@ const handleAddItem = (e) => {
 
   const handleRemoveItem = (e) => {
 
-    const elements = Array.from(document.querySelectorAll(`.${e.target.className}`))
+    if(e.target.tagName === 'BUTTON') {
 
-    const filterElement = elements.filter(item => {
-      return item.id == e.target.id
-    })
+      const elements = Array.from(document.querySelectorAll(`.${e.target.className}`))
 
-    menuArray.map(item => {
-      if(item.id == e.target.id) {
-        totalPrice -= item.price
-        totalPriceEle.textContent = `$${totalPrice}` 
-      }
-    })
+      const filterElement = elements.filter(item => {
+        return item.id == e.target.id
+      })
 
-    filterElement[0].parentElement.remove()
+      menuArray.map(item => {
+        if(item.id == e.target.id) {
+          totalPrice -= item.price
+          totalPriceEle.textContent = `$${totalPrice}` 
+        }
+      })
 
-     
+      filterElement[0].parentElement.remove()
+    }
   }
   
 
 document.getElementById('menu').addEventListener('click', handleAddItem)
 
 document.getElementById('items').addEventListener('click', handleRemoveItem)
+
+// Modal 
+
+const openModal = function () {
+  document.getElementById('modal').style.display = 'flex'
+  overlay.classList.remove("hidden")
+};
+
+const closeModal = function () {
+  document.getElementById('modal').style.display = 'none'
+  modal.classList.add("hidden")
+  overlay.classList.add("hidden")
+}
+
+openModalBtn.addEventListener("click", openModal)
+closeModalBtn.addEventListener("click", closeModal)
+overlay.addEventListener("click", closeModal);
