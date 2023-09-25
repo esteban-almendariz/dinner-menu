36c6ba20145f239ff41221f93f119783fb819d5b
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2023-09-24 20:33:18 -0500

    trying to fix images on vercel website

--- data.js
+++ data.js
@@ -4,20 +4,20 @@ const menuArray = [
         ingredients: ["pepperoni, mushrom, mozarella"],
         id: 0,
         price: 14,
-        emoji: '/images/pizza.png'
+        emoji: './images/pizza.png'
     },
     {
         name: "Hamburger",
         ingredients: ["beef, cheese, lettuce"],
         price: 12,
-        emoji: '/images/burger.png',
+        emoji: './images/burger.png',
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 10,
-        emoji: '/images/beer.png',
+        emoji: './images/beer.png',
         id: 2
     }
 ]
